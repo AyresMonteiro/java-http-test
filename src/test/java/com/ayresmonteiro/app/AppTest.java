e832908ba750d6f4798a97540177d5875d40a8ab
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-16 17:53:24 -0300

    doing some integration tests with HTTPRequestHandler

--- src/test/java/com/ayresmonteiro/app/AppTest.java
+++ src/test/java/com/ayresmonteiro/app/AppTest.java
@@ -1,20 +1,34 @@
 package com.ayresmonteiro.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+
+import com.ayresmonteiro.app.lib.http.HTTPRequestHandler;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void assertResponseIsNotNull() {
+        HttpResponse<String> response = HTTPRequestHandler
+                .doJSONRequest("https://pokeapi.co/api/v2/pokemon/charizard/", null);
+
+        assertNotEquals(null, response);
+    }
+
+    @Test
+    public void assertResponseIsString() {
+        HttpResponse<String> response = HTTPRequestHandler
+                .doJSONRequest("https://pokeapi.co/api/v2/pokemon/charizard/", null);
+
+        assertEquals(" ".getClass(), response.body().getClass());
     }
 }
