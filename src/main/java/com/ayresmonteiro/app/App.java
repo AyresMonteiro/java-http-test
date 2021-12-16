bc3989ab00249492a5a11786bd6432dc9ec06c27
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-16 17:53:40 -0300

    using HTTPRequestHandler in project

--- src/main/java/com/ayresmonteiro/app/App.java
+++ src/main/java/com/ayresmonteiro/app/App.java
@@ -1,13 +1,22 @@
 package com.ayresmonteiro.app;
 
+import java.net.http.HttpResponse;
+
+import com.ayresmonteiro.app.lib.http.HTTPRequestHandler;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        HttpResponse<String> response = HTTPRequestHandler
+                .doJSONRequest("https://pokeapi.co/api/v2/pokemon/charizard/", null);
+
+        if (response != null) {
+            System.out.println(response.body());
+        } else {
+            System.out.println("Null");
+        }
     }
 }
