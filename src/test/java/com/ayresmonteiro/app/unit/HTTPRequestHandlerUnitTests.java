532fda88a08f8a1ff126c23be128ba75a14e8adf
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-16 20:09:10 -0300

    creates test assertIfURIWasBuildedCorrectly()

--- src/test/java/com/ayresmonteiro/app/unit/HTTPRequestHandlerUnitTests.java
+++ src/test/java/com/ayresmonteiro/app/unit/HTTPRequestHandlerUnitTests.java
@@ -3,8 +3,11 @@ package com.ayresmonteiro.app.unit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+import java.net.http.HttpRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.junit.Test;
 
@@ -32,5 +35,24 @@ public class HTTPRequestHandlerUnitTests {
         headers = HTTPRequestHandler.addAcceptJSONToHeaders(null);
 
         assertEquals("application/json", headers.get("accept"));
+    }
+
+    @Test
+    public void assertIfURIWasBuildedCorrectly() {
+        String[] availableURIs = {
+                "https://google.com",
+                "https://facebook.com",
+                "https://twitter.com",
+        };
+
+        Random rand = ThreadLocalRandom.current();
+
+        String selectedURI = availableURIs[rand.nextInt((availableURIs.length - 0))];
+
+        Map<String, String> headers = HTTPRequestHandler.addAcceptJSONToHeaders(null);
+
+        HttpRequest request = HTTPRequestHandler.createBaseRequestBuilder(selectedURI, headers).build();
+
+        assertEquals(selectedURI, request.uri().toString());
     }
 }
