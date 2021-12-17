6e11df5da96cde0ec3f9d46a3cd464343b0f01e1
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-17 07:35:15 -0300

    App.java is just an example

--- src/main/java/com/ayresmonteiro/app/App.java
+++ src/main/java/com/ayresmonteiro/app/App.java
@@ -2,6 +2,7 @@ package com.ayresmonteiro.app;
 
 import java.net.http.HttpResponse;
 
+import com.ayresmonteiro.app.lib.format.JSONableHashMap;
 import com.ayresmonteiro.app.lib.http.HTTPRequestHandler;
 
 /**
@@ -10,13 +11,29 @@ import com.ayresmonteiro.app.lib.http.HTTPRequestHandler;
  */
 public class App {
     public static void main(String[] args) {
+        JSONableHashMap<String, String> body = new JSONableHashMap<String, String>() {
+            {
+                put("email", "any@mail.com");
+                // ... More Data
+            }
+        };
+
         HttpResponse<String> response = HTTPRequestHandler
-                .doGetJSONRequest("https://pokeapi.co/api/v2/pokemon/charizard/", null);
+                .doPostJSONRequest("http://localhost/post/route", null, body.toJson());
+
+        if (response != null) {
+            System.err.println(response.body());
+        } else {
+            System.err.println("POST: Null");
+        }
+
+        response = HTTPRequestHandler
+                .doGetJSONRequest("http://localhost/get/route", null);
 
         if (response != null) {
             System.out.println(response.body());
         } else {
-            System.out.println("Null");
+            System.out.println("GET: Null");
         }
     }
 }
